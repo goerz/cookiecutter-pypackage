e3e743d2f717f09bbacf273af0a912e9dca0c9f1
Michael Goerz <goerz@stanford.edu>
2019-04-22 20:02:52 -0400

    Make tests black-compliant

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -8,4 +8,6 @@ import {{ cookiecutter.project_slug }}
 
 def test_valid_version():
     """Check that the package defines a valid __version__"""
-    assert parse_version({{ cookiecutter.project_slug }}.__version__) >= parse_version("{{ cookiecutter.version }}")
+    assert parse_version({{ cookiecutter.project_slug }}.__version__) >= parse_version(
+        "{{ cookiecutter.version }}"
+    )
